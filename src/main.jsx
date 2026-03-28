ecb03898b587a2850c4a7a097fda416bfe0c0ba8
melavinc <vincentimelissa59@gmail.com>
2026-03-28 23:12:44 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './src/App.jsx'    // Entra in src per trovare App
-import './src/index.css'           // Entra in src per trovare il CSS
+import App from './src/App.jsx'
+import './src/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
