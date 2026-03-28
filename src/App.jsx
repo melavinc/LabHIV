dca5da8448b6ad528c8e1d135967b8968566ae3d
melavinc <vincentimelissa59@gmail.com>
2026-03-28 22:25:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { Toaster } from "./components/ui/toaster" // rimosso @
+import { Toaster } from "./components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClientInstance } from './lib/query-client' // rimosso @
+import { queryClientInstance } from './lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
-import { AuthProvider, useAuth } from './lib/AuthContext'; // rimosso @
-import UserNotRegisteredError from './components/UserNotRegisteredError'; // rimosso @
-// Add page imports here
+import { AuthProvider, useAuth } from './lib/AuthContext';
+import UserNotRegisteredError from './components/UserNotRegisteredError';
 import Game from './pages/Game';
 
 const AuthenticatedApp = () => {
