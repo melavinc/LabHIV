b7e6afba913578fdc60b61c7aa84a5e08a8f9611
melavinc <vincentimelissa59@gmail.com>
2026-03-28 22:24:07 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import App from './src/App.jsx'    // Entra in src per trovare App
+import './src/index.css'           // Entra in src per trovare il CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
